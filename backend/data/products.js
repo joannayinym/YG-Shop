d4c1a112310e94bcd35f0eb04f92bb199c7ff856
joannayinym <joannayinym@gmail.com>
2021-01-20 14:30:04 +1100

    prepare to deployment

---
+++ backend/data/products.js
@@ -0,0 +1,76 @@
+const products = [
+  {
+    name: 'Airpods Wireless Bluetooth Headphones',
+    image: '/images/products_1.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatible devices wirelessly Hogh-quality AAC audio offers immersive listening experience Built-in mcophone allows you to take calls while working',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.99,
+    countInStock: 0,
+    rating: 3.5,
+    numReviews: 4,
+  },
+  {
+    name: 'Airpods Wireless Bluetooth Headphones',
+    image: '/images/products_2.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatible devices wirelessly Hogh-quality AAC audio offers immersive listening experience Built-in mcophone allows you to take calls while working',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.99,
+    countInStock: 3,
+    rating: 4.0,
+    numReviews: 4,
+  },
+  {
+    name: 'Airpods Wireless Bluetooth Headphones',
+    image: '/images/products_3.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatible devices wirelessly Hogh-quality AAC audio offers immersive listening experience Built-in mcophone allows you to take calls while working',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.99,
+    countInStock: 3,
+    rating: 2.5,
+    numReviews: 4,
+  },
+  {
+    name: 'Airpods Wireless Bluetooth Headphones',
+    image: '/images/products_4.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatible devices wirelessly Hogh-quality AAC audio offers immersive listening experience Built-in mcophone allows you to take calls while working',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.99,
+    countInStock: 3,
+    rating: 4.5,
+    numReviews: 4,
+  },
+  {
+    name: 'Airpods Wireless Bluetooth Headphones',
+    image: '/images/products_5.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatible devices wirelessly Hogh-quality AAC audio offers immersive listening experience Built-in mcophone allows you to take calls while working',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.99,
+    countInStock: 3,
+    rating: 3.0,
+    numReviews: 4,
+  },
+  {
+    name: 'Airpods Wireless Bluetooth Headphones',
+    image: '/images/products_6.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatible devices wirelessly Hogh-quality AAC audio offers immersive listening experience Built-in mcophone allows you to take calls while working',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.99,
+    countInStock: 3,
+    rating: 4.5,
+    numReviews: 4,
+  },
+];
+
+export default products;
---
+++ frontend/src/reducers/productReducers.js
@@ -0,0 +1,106 @@
+import * as productActionTypes from '../constants/productConstants';
+
+export const productListReducer = (state = { products: [] }, action) => {
+  switch (action.type) {
+    case productActionTypes.PRODUCT_LIST_REQUEST:
+      return { loading: true, products: [] };
+    case productActionTypes.PRODUCT_LIST_SUCCESS:
+      return {
+        loading: false,
+        products: action.payload.products,
+        page: action.payload.page,
+        pages: action.payload.pages,
+      };
+    case productActionTypes.PRODUCT_LIST_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const productDetailsReducer = (
+  state = { product: { reviews: [] } },
+  action
+) => {
+  switch (action.type) {
+    case productActionTypes.PRODUCT_DETAILS_REQUEST:
+      return { loading: true, ...state };
+    case productActionTypes.PRODUCT_DETAILS_SUCCESS:
+      return { loading: false, product: action.payload };
+    case productActionTypes.PRODUCT_DETAILS_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const productDeleteReducer = (state = {}, action) => {
+  switch (action.type) {
+    case productActionTypes.PRODUCT_DELETE_REQUEST:
+      return { loading: true };
+    case productActionTypes.PRODUCT_DELETE_SUCCESS:
+      return { loading: false, success: true };
+    case productActionTypes.PRODUCT_DELETE_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const productCreateReducer = (state = {}, action) => {
+  switch (action.type) {
+    case productActionTypes.PRODUCT_CREATE_REQUEST:
+      return { loading: true };
+    case productActionTypes.PRODUCT_CREATE_SUCCESS:
+      return { loading: false, success: true, product: action.payload };
+    case productActionTypes.PRODUCT_CREATE_FAIL:
+      return { loading: false, error: action.payload };
+    case productActionTypes.PRODUCT_CREATE_RESET:
+      return {};
+    default:
+      return state;
+  }
+};
+
+export const productUpdateReducer = (state = {}, action) => {
+  switch (action.type) {
+    case productActionTypes.PRODUCT_UPDATE_REQUEST:
+      return { loading: true };
+    case productActionTypes.PRODUCT_UPDATE_SUCCESS:
+      return { loading: false, success: true, product: action.payload };
+    case productActionTypes.PRODUCT_UPDATE_FAIL:
+      return { loading: false, error: action.payload };
+    case productActionTypes.PRODUCT_UPDATE_RESET:
+      return { product: {} };
+    default:
+      return state;
+  }
+};
+
+export const productReviewCreateReducer = (state = {}, action) => {
+  switch (action.type) {
+    case productActionTypes.PRODUCT_CREATE_REVIEW_REQUEST:
+      return { loading: true };
+    case productActionTypes.PRODUCT_CREATE_REVIEW_SUCCESS:
+      return { loading: false, success: true };
+    case productActionTypes.PRODUCT_CREATE_REVIEW_FAIL:
+      return { loading: false, error: action.payload };
+    case productActionTypes.PRODUCT_CREATE_REVIEW_RESET:
+      return {};
+    default:
+      return state;
+  }
+};
+
+export const productTopRatedReducer = (state = {}, action) => {
+  switch (action.type) {
+    case productActionTypes.PRODUCT_TOP_REQUEST:
+      return { loading: true, products: [] };
+    case productActionTypes.PRODUCT_TOP_SUCCESS:
+      return { loading: false, products: action.payload };
+    case productActionTypes.PRODUCT_TOP_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
